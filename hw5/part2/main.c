183db3bfcad4e71edaf830bffea84cc06939de99
yenye0630 <game02555@gmail.com>
2023-05-21 21:43:31 +0800

    modify main.c

--- hw5/part2/main.c
+++ hw5/part2/main.c
@@ -5,36 +5,33 @@ int main()
 {
 
     pid_t pid = fork();
-    while (1)
+    if (pid > 0) /*parent mode*/
     {
-        if (pid > 0) /*parent mode*/
-        {
-            waitpid(pid, NULL, 0);
-            // test_read("foo", context);
-            // printf("READ: %s\n", context);
+        waitpid(pid, NULL, 0);
+        // test_read("foo", context);
+        // printf("READ: %s\n", context);
 
-            char buf[sysconf(_SC_PAGESIZE)];
-            test_read("foo", buf);
-            printf("READ: %s\n", buf);
-        }
-        else if (pid == 0) /*child mode*/
-        {
-            size_t _len = 0;
-            printf("WRITE: \n");
-            while (~scanf("%[^\n]s", context) && getchar())
-            {
-                size_t len = strlen(context);
-                context[len++] = '\n';
-                _len += len;
-                break;
-            }
-            test_write("foo", context);
-        }
-        else
-        {
-            perror("fork error.\n");
-            exit(EXIT_FAILURE);
-        }
-        return 0;
+        char buf[sysconf(_SC_PAGESIZE)];
+        test_read("foo", buf);
+        printf("READ: %s", buf);
     }
+    else if (pid == 0) /*child mode*/
+    {
+        size_t _len = 0;
+        printf("WRITE: \n");
+        while (~scanf("%[^\n]s", context) && getchar())
+        {
+            size_t len = strlen(context);
+            context[len++] = '\n';
+            _len += len;
+            break;
+        }
+        test_write("foo", context);
+    }
+    else
+    {
+        perror("fork error.\n");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
 }
